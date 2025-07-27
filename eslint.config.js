1e5a8991586512016b93956f8e91c086c8f1d542
Kazuho1222 <destiny5914@gmail.com>
2025-07-27 10:22:05 +0900

    007-eslint-jsconfig

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import js from "@eslint/js";
+
+export default [
+    js.configs.recommended,
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            ecmaVersion: 2020,
+            sourceType: "module",
+            globals: {
+                // AMD
+                define: "readonly",
+                require: "readonly",
+                // Browser
+                window: "readonly",
+                document: "readonly",
+                console: "readonly",
+                // ES6
+                Promise: "readonly",
+                Set: "readonly",
+                Map: "readonly",
+                Symbol: "readonly",
+                Proxy: "readonly",
+                Reflect: "readonly",
+            },
+        },
+        rules: {
+            indent: ["error", 2],
+            quotes: ["warn", "single"],
+            semi: ["warn", "never"],
+            "no-unused-vars": [
+                "error",
+                { vars: "all", args: "after-used", ignoreRestSiblings: true },
+            ],
+            "comma-dangle": ["warn", "always-multiline"],
+        },
+    },
+];
