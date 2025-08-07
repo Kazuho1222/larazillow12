95eeb648ebb3c816705dac0554a6aba3c7bae73c
Kazuho1222 <destiny5914@gmail.com>
2025-08-08 08:44:06 +0900

    007-001-auth-controller-and-routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
@@ -11,3 +12,10 @@ Route::get(
 Route::get('/hello', [IndexController::class, 'show']);
 
 Route::resource('listing', ListingController::class);
+
+Route::get('login', [AuthController::class, 'create'])
+    ->name('login');
+Route::get('login', [AuthController::class, 'store'])
+    ->name('login.store');
+Route::get('login', [AuthController::class, 'destroy'])
+    ->name('logout');
