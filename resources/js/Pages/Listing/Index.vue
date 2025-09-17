40bfb38638dd622f9a19275c9ec31e9d9d9c5380
Kazuho1222 <destiny5914@gmail.com>
2025-09-18 08:52:05 +0900

    009-002-filtering-form-markup

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,4 +1,5 @@
 <template>
+    <Filters />
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
         <Listing
             v-for="listing in listings.data"
@@ -17,6 +18,7 @@
 <script setup>
 import Pagination from "@/Components/UI/Pagination.vue";
 import Listing from "@/Pages/Listing/Index/Components/Listing.vue";
+import Filters from "./Index/Components/Filters.vue";
 
 defineProps({
     listings: Object,
