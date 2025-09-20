53b255b82ceea6d818adb3d18a81b4fbe23582bb
Kazuho1222 <destiny5914@gmail.com>
2025-09-20 13:10:00 +0900

    010-003-user-listings

--- app/Http/Controllers/RealtorListingController.php
+++ app/Http/Controllers/RealtorListingController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class RealtorListingController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Realtor/Index');
+        return Inertia::render(
+            'Realtor/Index',
+            ['listings' => Auth::user()->listings]
+        );
     }
 }
