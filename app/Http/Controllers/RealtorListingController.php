3e073ceab244dbc43dfe1711468af1e73ba3e6bc
Kazuho1222 <destiny5914@gmail.com>
2025-09-22 10:20:00 +0900

    010-009-filtering-backend-bools

--- app/Http/Controllers/RealtorListingController.php
+++ app/Http/Controllers/RealtorListingController.php
@@ -16,11 +16,21 @@ class RealtorListingController extends Controller
     //     $this->authorizeResource(Listing::class, 'listing');
     // }
 
-    public function index()
+    public function index(Request $request)
     {
+        $filters = [
+            'deleted' => $request->boolean('deleted')
+        ];
+
         return Inertia::render(
             'Realtor/Index',
-            ['listings' => Auth::user()->listings]
+            [
+                'listings' => Auth::user()
+                    ->listings()
+                    ->mostRecent()
+                    ->filter($filters)
+                    ->get()
+            ]
         );
     }
 
