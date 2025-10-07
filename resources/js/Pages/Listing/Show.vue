13e0788ad8d0c8bac40dd9ee12d9e12ec30de88f
Kazuho1222 <destiny5914@gmail.com>
2025-10-08 08:30:27 +0900

    012-004-vue-custom-component-events

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -80,6 +80,7 @@
                 v-if="user"
                 :listing-id="listing.id"
                 :price="listing.price"
+                @offer-updated="offer = $event"
             />
         </div>
     </div>
@@ -102,8 +103,10 @@ const props = defineProps({
     listing: Object,
 });
 
+const offer = ref(props.listing.price);
+
 const { monthlyPayment, totalPaid, totalInterest } = useMonthlyPayment(
-    props.listing.price,
+    offer,
     interestRate,
     duration
 );
